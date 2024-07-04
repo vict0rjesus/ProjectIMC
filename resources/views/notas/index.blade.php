ea53bbfbefda98df09506ab36a18f94b97c0820d
Jean Victor Alves de Jesus <jean3166796@aluno.ba.senac.br>
2024-07-03 21:01:22 -0300

    03/07.3

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -1,5 +1,28 @@
 <x-layout title=Notas>
 
-    <h1>TESTE NOTAS</h1>
+<div class="container">
+<form method="post" action="">
+  @csrf
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label" placeholder="Nome">Nome</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" name="nome">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label" placeholder="media">Media</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" name="media">
+    </div>
+  </div>
+  <select class="form-select" aria-label="Default select example" name="status">
+   <option selected>Status do aluno</option>
+   <option value="1">Aprovado</option>
+   <option value="2">Reprovado</option>
+  </select><br>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+</div>
 
 </x-layout>
